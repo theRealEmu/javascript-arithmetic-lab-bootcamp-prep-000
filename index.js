7bb07e4011a296544d6414ef38ec5b22cbf3a89e
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-12 09:16:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //Fu add
 function add(num1, num2) {
-  return num1 + num2;
+  
+  if (parseInt(num1) && parseInt(num2)) {
+    return parseInt(num1,10) + parseInt(num2);
+  }
+  
+  console.log("Please enter numbers only");
+  
 }
 
 
